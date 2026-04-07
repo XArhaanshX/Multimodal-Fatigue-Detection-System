343946c9290d6a400f442541a18e4a64cc7a7f26
XArhaanshX <arhaanshjhingan@gmail.com>
2026-04-07 12:50:34 +0530

    initial structure push

---
+++ backend/vision/camera.py
@@ -0,0 +1,16 @@
+import cv2
+
+def start_camera():
+    cap = cv2.VideoCapture(0)
+
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        yield frame
+
+    cap.release()
